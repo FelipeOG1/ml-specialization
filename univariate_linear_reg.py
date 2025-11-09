aef814786d3c8309ca5eb3149ea5e069fd20754d
FelipeOG1 <feliog1234@gmail.com>
2025-11-08 19:46:08 -0600

    [example1] add mean uses 50 m samples

--- univariate_linear_reg.py
+++ univariate_linear_reg.py
@@ -24,27 +24,26 @@ import matplotlib.pyplot as plt
 
 class UniLinearRegression:
     def __init__(self,learning_rate:float):
-        self.x_train = np.array([1.0,2.0])
-        self.y_train = np.array([300.0,500.0])
-        self.m = len(self.x_train)
+        np.random.seed(0)
+        self.m = 50
+        self.x_train = np.linspace(0, 10, self.m)                  
+        self.y_train = 3 * self.x_train + 4 + np.random.randn(self.m) * 2
         self.w = 0
         self.b = 0 
         self.y_hat = lambda x_train: self.w * x_train + self.b
         self.learning_rate = learning_rate 
         self.mean_normalization = lambda train: (train - np.mean(train)) / (np.max(train) - np.min(train))
-        self.y_train = self.mean_normalization(self.y_train)
-    def compute_vectorized_cost_function(self):return ((self.y_hat(self.x_train) - self.y_train) **2).sum() / (2 * self.m)
-    def compute_cost_function(self):return sum([(self.y_hat(x) - y) **2 for x,y in zip(self.x_train,self.y_train)]) / (2 * self.m)
-    def compute_w_derivative(self):return sum([(self.y_hat(x) - y) * x for x,y in zip (self.x_train,self.y_train)]) / self.m
-    def compute_b_derivative(self):return sum([(self.y_hat(x) - y) for x,y in zip (self.x_train,self.y_train)]) / self.m
+    def compute_cost_function(self):return ((self.y_hat(self.x_train) - self.y_train) ** 2).sum() / (2 * self.m)
+    def compute_w_derivative(self):return ((self.y_hat(self.x_train) - self.y_train) * self.x_train).sum() / self.m
+    def compute_b_derivative(self):return (self.y_hat(self.x_train) - self.y_train).sum() / self.m
 
 
-    def gradient_descent(self,max_iter = 1000,epsilon = 1e-6,prev_cost = float("inf")):
+    def gradient_descent(self,max_iter = 100000,epsilon = 1e-6,prev_cost = float("inf")):
         for i in range(max_iter):
             temp_w = self.w - self.learning_rate * self.compute_w_derivative()
             temp_b = self.b - self.learning_rate * self.compute_b_derivative()
             self.w,self.b = temp_w,temp_b
-            current_cost = self.compute_vectorized_cost_function()
+            current_cost = self.compute_cost_function()
             print(f"prev_cost:{prev_cost},current_cost:{current_cost}")          
             if abs(current_cost - prev_cost) < epsilon:
                 print(f"w and b converge on {i} iteration")
@@ -68,12 +67,10 @@ class UniLinearRegression:
         plt.ylabel('Price (in 1000s of dollars)')
         plt.xlabel('Size (1000 sqft)')
         plt.savefig("graph.png")
-
     
     def main(self):
         self.gradient_descent()
         self._draw_graph(self.compute_model_output())
-           
     
         
 ulr = UniLinearRegression(0.0001)
