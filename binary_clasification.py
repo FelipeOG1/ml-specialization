8501258db176039500c214662438ff6906a0460b
FelipeOG1 <feliog1234@gmail.com>
2025-12-26 19:04:50 -0600

    [binary clasification] fix w shape

--- binary_clasification.py
+++ binary_clasification.py
@@ -19,26 +19,25 @@ class BinaryClasification:
         self.y = np.array([0,0,0,1,1,1]).reshape(-1,1)#reshape asserts 2 dimension array for predictions
         self.m,self.n= self.X.shape
         self.b = -3
-        self.w = np.zeros(self.m)#zeros with len features
+        self.w = np.zeros((self.n,1))#zeros with len features
         self.alpha = 0.0001
         self.lam = 200000#lambda value for regularization
-        self.REG = self.lam/(2*self.m) * np.sum(np.square(self.w))
+        self.REG_COST_FUNCTION = self.lam/(2*self.m) * np.sum(np.square(self.w))
         self.REG_DERIVATIVE = (self.lam / self.m)*self.w
+        
     def compute_z(self)->NDArray:return np.dot(self.X,self.w) + self.b
-    def compute_logistic_function(self,z:NDArray)->NDArray:return 1/(1+np.exp(-z))
+    def compute_sigmoid(self,z:NDArray)->NDArray:return 1/(1+np.exp(-z))
     def compute_w_derivative(self,predictions):return np.mean(np.dot(self.X.T,(predictions - self.y)))
     def compute_b_derivative(self,predictions):return np.mean(predictions - self.y) 
     def compute_losses(self,predictions): return -self.y*np.log(predictions) - (1-self.y)*np.log(1 - predictions)
     def compute_cost_function(self,predictions):return np.mean(self.compute_losses(predictions))
-    
-
     
         
     def gradient_descent(self,epsilon = 1e-6,max_iterations = 20000):
         prev_cost = float('inf')
         cost = 0
         for i in range(max_iterations):
-            predictions = self.compute_logistic_function(self.compute_z())
+            predictions = self.compute_sigmoid(self.compute_z())
             dw = self.compute_w_derivative(predictions)  # Shape: (n_features,)
             db = self.compute_b_derivative(predictions)  # Shape: scalar
             self.w -= self.alpha *dw
@@ -51,9 +50,25 @@ class BinaryClasification:
         return cost 
 
        
+    def regularize_gradient_descent(self,epsilon = 1e-6,max_iterations = 20000):
+        prev_cost = float('inf')
+        cost = 0
+        for i in range(max_iterations):
+            predictions = self.compute_sigmoid(self.compute_z())
+            dw = self.compute_w_derivative(predictions) + self.REG_DERIVATIVE
+            db = self.compute_b_derivative(predictions) #b doesnt regularize
+            self.w -= self.alpha *dw
+            self.b -=self.alpha * db
+            cost = self.compute_cost_function(predictions) + self.REG_COST_FUNCTION
+            print(cost)
+            if abs(prev_cost - cost) < epsilon:
+                break
+            prev_cost = cost
+        return cost 
 
+ 
     
     def __call__(self):
-        return self.n
+        return self.regularize_gradient_descent()
 bn = BinaryClasification()
 print(bn())
