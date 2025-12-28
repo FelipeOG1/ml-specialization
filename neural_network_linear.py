df95171b6e9b6f9140a2412a8d014d86d8593c40
FelipeOG1 <feliog1234@gmail.com>
2025-12-28 14:33:38 -0600

    [Neural Network] complete sigmoid layer with single neuron

--- neural_network_linear.py
+++ neural_network_linear.py
@@ -1,41 +1,53 @@
 
-
-
 from decimal import DecimalTuple
 import numpy as np
-from numpy._core.numeric import dtype
 import tensorflow as tf
-from tensorflow.keras.layers import Dense, Input
-from tensorflow.keras import Sequential
-from tensorflow.keras.losses import MeanSquaredError, BinaryCrossentropy
-from tensorflow.keras.activations import sigmoid
-from tensorflow.python.ops.gen_experimental_dataset_ops import weighted_flat_map_dataset
+
+
+    
+    
+
+
 class NeuralNetwork:
     def __init__(self,X,y_train):
         self.X = X
         self.y_train = y_train
-        self.layer = tf.keras.layers.Dense(units = 1,activation = 'linear')
-        self.layer.build(input_shape = (None,1))#need this for custom weights
+        self.linear_layer = tf.keras.layers.Dense(units = 1,activation = 'linear')
+        self.linear_layer.build(input_shape = (None,1))#need this for custom weights
+        
+        self.model = tf.keras.Sequential([
+            tf.keras.layers.Dense(units = 1,input_dim = 1,activation = 'sigmoid',name = "L1")
+            ])
+
         
     @property
-    def weights(self):return self.layer.get_weights()
-    
+    def weights(self):
+        kernel_w,kernel_b = self.model.weights
+        return kernel_w.numpy(),kernel_b.numpy()
+        
     def setup_custom_weights(self,w:float,b:float)->None:
         set_w = np.array([[w]],dtype = np.float32)
         set_b = np.array([b],dtype = np.float32)
-        self.layer.set_weights([set_w,set_b])
-        assert(self.layer.get_weights() == [[set_w],[set_b]])
+        self.linear_layer.set_weights([set_w,set_b])
+        self.model.get_layer('L1').set_weights([set_w,set_b])
+        assert(self.linear_layer.get_weights() == [[set_w],[set_b]])
         
-    def test_linear_regression(self):
-        w,b = self.layer.get_weights()
-        a1 = self.layer(self.X[0].reshape(1,1))
+    def test_linear_layer_activation(self):
+        w,b = self.linear_layer.get_weights()
+        a1 = self.linear_layer(self.X[0].reshape(1,1))
         manual_a1 = np.dot(self.X[0].reshape(1,1),w) + b
         assert(a1 == manual_a1)
-        
+    def test_sigmoid_activation(self):
+        w,b = self.weights
+        sigmoid = lambda z:1/(1+np.exp(-z))
+        a1 = self.model.predict(self.X[0].reshape(1,1))
+        alog = sigmoid(np.dot(self.X[0].reshape(1,1),w) + b)
+        assert(a1 == alog)
 x =  np.array([[1.0], [2.0]], dtype=np.float32)#a0
 y = np.array([[300.0], [500.0]], dtype=np.float32)
 nn = NeuralNetwork(x,y)
 
-w,b = 200.0,100.0
+w,b = 2.0,-4.5
 nn.setup_custom_weights(w,b)
-print(nn.test_linear_regression())
+print(nn.test_sigmoid_activation())
+
