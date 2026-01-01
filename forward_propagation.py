506cbc43f9deecdf7434031ddb6c778b156e5fe1
FelipeOG1 <feliog1234@gmail.com>
2025-12-31 20:11:12 -0600

    [forward propagation] finish example sigmoid

--- forward_propagation.py
+++ forward_propagation.py
@@ -1,18 +1,23 @@
 
-import Numpy as np
+
+import numpy as np
+import tensorflow as tf
+
+
 class Neuron:
-    def compute_scalar(self,x,w,b):return 
+    def compute_scalar(self,x,w,b):
         sigmoid = lambda z:1/(1+np.exp(-z))
         return sigmoid(-(np.dot(x,w)) + b)
     
 class Layer:
-    def __init__(self,units)
+    def __init__(self,units):
         self.units = units
         self.core = [Neuron() for _ in range(self.units)]
-        self.a_out = np.zeroes(units)
-        self.w =  np.array([[1,-3,5],[2,4,-6]])#fix w example
-        self.b = np.array([-1,1,2])
-    def __call__(self,x):return [self.core[i].compute_scalar(self.w[,:i],self.b[i],x) for i in range(len(self.core))]
+        self.a_out = np.zeros(units)
+        self.w =  np.array([[1,-3,5],[2,4,-6]],dtype = np.float32)#fix w example
+        self.b = np.array([-1,1,2],dtype = np.float32)
+        
+    def __call__(self,x):return [self.core[i].compute_scalar(self.w[:,i],self.b[i],x) for i in range(len(self.core))]
        
         
     def set_weights(self,w,b):self.w,self.b = w,b
@@ -23,7 +28,14 @@ class Sequential:
         self.layers = layers
         
     def predict(self,x):
-        layers_predictions = [self.layer[0](x)]
+        
+           
+            
+        
         
 
-
+if __name__ == "__main__":
+    l1 = Layer(units = 3)
+    x_train = np.array([1.0,3.0])
+    prediction = l1(x_train)
+    print(prediction)
