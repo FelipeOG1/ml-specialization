93ace3b3cb62d9976d1a9276c95a5fb21f4696be
FelipeOG1 <feliog1234@gmail.com>
2026-01-01 21:17:50 -0600

    [Forward Propagation]

--- forward_propagation.py
+++ forward_propagation.py
@@ -1,5 +1,3 @@
-
-
 import numpy as np
 import tensorflow as tf
 
@@ -17,8 +15,11 @@ class Layer:
         self.w =  np.array([[1,-3,5],[2,4,-6]],dtype = np.float32)#fix w example
         self.b = np.array([-1,1,2],dtype = np.float32)
         
-    def __call__(self,x):return [self.core[i].compute_scalar(self.w[:,i],self.b[i],x) for i in range(len(self.core))]
-       
+    def __call__(self,x):
+        predictions = np.array([
+            self.core[i].compute_scalar(self.w[:,i],self.b[i],x) for i in range(len(self.core))])
+        self.a_out = predictions
+        return self.a_out
         
     def set_weights(self,w,b):self.w,self.b = w,b
             
@@ -28,12 +29,14 @@ class Sequential:
         self.layers = layers
         
     def predict(self,x):
-        
-           
-            
-        
-        
+       prev_a = x
+       for layer in self.layers:
+           a = layer(prev_a)
+           prev_a = a
 
+        return layer[-1].a_out
+           
+    
 if __name__ == "__main__":
     l1 = Layer(units = 3)
     x_train = np.array([1.0,3.0])
