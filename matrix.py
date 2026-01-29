5c2b553614239c860b4d2b17e8d952b8fb2f3a69
FelipeOG1 <feliog1234@gmail.com>
2026-01-29 17:01:29 -0600

    Add simple matrix validations

--- matrix.py
+++ matrix.py
@@ -1,30 +1,29 @@
 
+"""
 
+It has super weak rules for matrix validation 
+This is just to help me understand the concectps behind  np.dot, matmul,etc.
 
+"""
 class Matrix:
+
     def __init__(self,values:list) -> None:
         if not all(isinstance(value,list) for value in values):
             raise ValueError("invalid matrix")
-
+        if not all(len(values[i]) == len(values[0]) for i in range(1,len(values),1)):
+            raise ValueError("Invalid matrix")
+    
 
         self.values = values 
-        
-    
-    @property
-    def shape(self):
-        base_len = len(self.values[0])
-        if any(len(value) != base_len for value in self.values[1:]):
-            raise ValueError("invalid sequence")
 
+    def __add__(self,other):
+       if not isinstance(other,Matrix):
+           raise TypeError(f"Cannot sum matrix and {type(other)}")
 
-        
-        
-    
-        
     
 
 
-
-
-
+m1 = Matrix([[1,3,3],
+             [3,3,3]])
+   
 
