8755e7238434a8424f3e94c1620a854ae9f9f69c
FelipeOG1 <feliog1234@gmail.com>
2026-01-30 10:02:38 -0600

    Finish dot

--- matrix.py
+++ matrix.py
@@ -16,15 +16,10 @@ class Matrix:
     def T(self):
         rows,cols = self.shape
         return Matrix([[row[i] for row in self.values] for i in range(cols)])
-    def get_column(self,col):
-        rows,cols = self.shape
 
-        if col>cols:
-            raise ValueError("Invalid col position")
-        return [row[col] for row in self.values]
+    def column(self,col):return [row[col] for row in self.values]
+    
             
-            
-        
     def __repr__(self) -> str:
         return f"Matrix({self.values})"
 
@@ -74,13 +69,35 @@ class Matrix:
 
     
 
-    def dot(self,m2):
+    def dot_scalar(self,m2):
         new_matrix = self * m2
         _sum = 0
         for row in new_matrix:
             _sum+=sum(row)
 
         return _sum
-        
-    
-          
+
+    def dot_matrix(self,m2):
+        m1_rows,m1_cols = self.shape
+        m2_rows,m2_cols = m2.shape
+
+        valid = m1_cols == m2_rows
+        if not valid:
+            raise ValueError("Cannot operate dot")
+
+        new_values = []
+        for i in range(m1_rows):
+            row = self.values[i]
+            new_row = []
+            for j in range(m2_cols):
+                
+                col = m2.column(j)
+                new_scalar = sum([r * c for r,c in zip(row,col)])
+                new_row.append(new_scalar)
+
+                
+            new_values.append(new_row)
+
+
+        return Matrix(new_values)
+
