e24429f623ef7606415f11dc46749382773b75b9
FelipeOG1 <feliog1234@gmail.com>
2026-01-31 19:13:20 -0600

    Finish vectorized neural network

--- vectorized_sigmoid_network.py
+++ vectorized_sigmoid_network.py
@@ -2,20 +2,24 @@ import numpy as np
 from dataclasses import dataclass
 
 
+def g(z):return 1/(1+np.exp(-z))
+
 @dataclass
 class Layer:
     units:int
     w:np.ndarray | None = None
     b:np.ndarray | None = None
-    
-    
+      
 class Sequential:
     def __init__(self,layers:list[Layer]):
         self.layers = layers
 
     
-    def predict(self):
-        pass
+    def predict(self,x:np.ndarray):
+        a = x
+        for layer in self.layers:
+            a = g((a @ layer.w) + layer.b)
+        return a
 
     def __getitem__(self,position:int):
         return self.layers[position]
@@ -24,17 +28,4 @@ class Sequential:
         assert len(weights) == 2 * len(self.layers)
         for index,layer in enumerate(self.layers):
             layer.w,layer.b = weights[index * 2],weights[index * 2 + 1]
-
-model = Sequential([
-    Layer(units=25),
-    Layer(units=10)
-])
-
-a_in = np.random.rand(1000,400)
-
-w1,b1 = np.random.rand(400,25),np.random.rand(25,)
-
-w2,b2 = np.random.rand(25,10),np.random.rand(10,)
-
-model.set_weights([w1,b1,w2,b2])
 
