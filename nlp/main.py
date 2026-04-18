8d8d60e7d30df826c53160a169f46940424656f3
FelipeOG1 <feliog1234@gmail.com>
2026-04-18 17:42:18 -0600

    encode and decode chars

--- nlp/main.py
+++ nlp/main.py
@@ -2,7 +2,7 @@ import torch
 class NameTokenaizer:
     def __init__(self, names: list[str]):
         self._chars = sorted(list(set(''.join(names))))
-        self._char_index = {char:index for index, char in enumerate(self._chars)}
+        self._char_index = {char:index+1 for index, char in enumerate(self._chars)}
         self._index_char = {self._char_index[char]:char for _, char in enumerate(self._chars)}
         
         self._char_index["."] = 0
@@ -13,17 +13,34 @@ class NameTokenaizer:
     def num_chars(self):
         return len(self._chars) + 1
     
-    def encode(self, name: str):
-        return [self._char_index[char] for char in name]
-        
-    def decode(self, encoded_str: list[int]):
+    def encode_char(self, char: str):
+        return self._char_index[char]
+    def decode_int(self, value: int):
+        return self._index_char[value]
+    
+    def encode_string(self, name: str):
+        return [self._char_index[char] for char in name]   
+    def decode_list(self, encoded_str: list[int]):
         return ''.join([self._index_char[char] for char in encoded_str])
 
 class DigramMap:
     def __init__(self, names):
         self._tokenaizer = NameTokenaizer(names)
         self._tensor = torch.zeros(self._tokenaizer.num_chars, self._tokenaizer.num_chars)
+        self._generate_map()
         
+    def _generate_map(self):
+        for name in names:
+            chrs = '.' + name + '.'
+            for c1, c2 in zip(chrs, chrs[1:]):
+                self._tensor[self._tokenaizer.encode_char(c1), self._tokenaizer.encode_char(c2)] +=1
+        
+    def __getitem__(self, index: int):
+        return self._tensor[index]
+           
+
+
+    
 
 if __name__ == "__main__":
     import os
@@ -31,3 +48,4 @@ if __name__ == "__main__":
     names = open(data, "r").read().splitlines()
 
     digram = DigramMap(names)
+    print(digram[0])
