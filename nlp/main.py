a17270e86dc86cbe020ce0f4df1a2efd3ed766d2
FelipeOG1 <feliog1234@gmail.com>
2026-04-18 12:59:08 -0600

    use dot

--- nlp/main.py
+++ nlp/main.py
@@ -2,24 +2,32 @@ import torch
 import os
 names = open(os.path.join("data", "names.txt"), "r").read().splitlines()
 
-chars = list(set("".join(names)))
+chars = sorted(list(set("".join(names))))
 
-char_index = {char:i for i, char in enumerate(chars)}
+char_index = {char:i+1 for i, char in enumerate(chars)}
 index_char = {char_index[char]:char for _, char in enumerate(chars)}
 
-
-encoder = lambda text: [char_index[char] for char in text]
-decoder = lambda array: "".join([index_char[index] for index in array])
-pairs = lambda name: zip(name, name[1:])
+char_index["."] = 0
+index_char[0] = "."
+print(char_index)
 
 counts = {}
 for name in names:
-    chs = ['<S>'] + list(name) + ['<E>']
+    chs = ['.'] + list(name) + ['.']
     for c1, c2 in zip(chs, chs[1:]):
-        bigram = (c1, c2)
+        bigram = (char_index[c1], char_index[c2])
         counts[bigram] = counts.get(bigram, 0) + 1
 
 values = sorted(counts.items(), key=lambda x: -x[1])
-chars_c = len(chars) + 2
+chars_c = len(chars) + 1
 
 a = torch.zeros(chars_c, chars_c, dtype=torch.int32)
+
+for name in names:
+    for c1, c2 in zip(name, name[1:]):
+        idx1, idx2 = char_index[c1], char_index[c2]
+        a[idx1, idx2] += 1
+
+
+
+
