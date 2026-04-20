1d10c44d3d817e624aff4d90097336d6dd553ccc
FelipeOG1 <feliog1234@gmail.com>
2026-04-20 15:31:05 -0600

    training set

--- nlp/main.py
+++ nlp/main.py
@@ -1,10 +1,10 @@
 import torch
+
 class NameTokenaizer:
     def __init__(self, names: list[str]):
         self._chars = sorted(list(set(''.join(names))))
         self._char_index = {char:index+1 for index, char in enumerate(self._chars)}
         self._index_char = {self._char_index[char]:char for _, char in enumerate(self._chars)}
-        
         self._char_index["."] = 0
         self._index_char[0] = "."
         
@@ -24,13 +24,18 @@ class NameTokenaizer:
         return [self._char_index[char] for char in name]   
     def decode_list(self, encoded_str: list[int]):
         return ''.join([self._index_char[char] for char in encoded_str])
+    
+
+
 
 class DigramModel:
     def __init__(self, names):
+        self._data = names
         self._tokenaizer = NameTokenaizer(names)
         self._map = torch.zeros(self._tokenaizer.num_chars, self._tokenaizer.num_chars)
         self._generate_map()
         self.P = self._map / self._map.float().sum(1, keepdim=True)
+        
     def _generate_map(self):
         for name in names:
             name = self._tokenaizer.prepare_name(name) 
@@ -50,10 +55,31 @@ class DigramModel:
             log_lh += log_prob
             iterations += 1
         return -(log_lh / iterations)
+
+
+    def get_training_set(self) -> tuple[torch.tensor, torch.tensor]:
+        x, y = [], []
+        for name in self._data[:1]:
+            name = self._tokenaizer.prepare_name(name)
+            for c1, c2 in zip(name, name[1:]):    
+                x.append(self._tokenaizer.encode_char(c1)) 
+                y.append(self._tokenaizer.encode_char(c2))
+            
+
+        return torch.tensor(x), torch.tensor(y)
+                
+                  
+           
+    
+       
 if __name__ == "__main__":
     import os
     data = os.path.join("data", "names.txt")
     names = open(data, "r").read().splitlines()
+    
     m = DigramModel(names)
-    print(m.get_loss_name("emmanuel"))
+    
+    x, y = m.get_training_set()
+    print(x)
+    print(y)
     
---
+++ nlp/scripts.py
@@ -0,0 +1,7 @@
+from torch import Tensor
+
+def build_training_data(names: list[str]) -> tuple(Tensor, Tensor):
+    for name in names:
+        
+
+
